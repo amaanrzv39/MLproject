021ba13a9cb9fafcb03660a5dc36a61240fe6b6b
amaanrzv39 <amaan.rizvi39@gmail.com>
2024-02-11 23:12:10 +0530

    app.py added

--- src/utils.py
+++ src/utils.py
@@ -15,6 +15,13 @@ def saved_obj(file_path, obj):
 
     except Exception as e:
         raise CustomException(e, sys)
+
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return  pickle.load(file_obj)
+    except Exception as e:
+        raise CustomException(e, sys)
     
 def evaluate_models(X_train, y_train, X_test, y_test, models):
     try:
